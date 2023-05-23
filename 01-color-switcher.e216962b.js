97006e0f8b2242de54076e362add4017585b5bc2
AleksandrYemelianov <97186469+AleksandrYemelianov@users.noreply.github.com>
2023-05-23 20:36:02 +0000

    Deploying to gh-pages from @ AleksandrYemelianov/goit-js-hw-09@bbf9346a843fda9cce776babaf9514568d342263 🚀

---
+++ 01-color-switcher.e216962b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");let a=null;function r(){a=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.disabled=!0}t.addEventListener("click",(function(t){t.target===e&&r(),t.target===o&&(clearInterval(a),e.disabled=!1)}));
+//# sourceMappingURL=01-color-switcher.e216962b.js.map
